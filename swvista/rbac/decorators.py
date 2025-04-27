62f026e2ac4946b6a37b0895fbf446bc869a0332
sachin pawar <sachinpawar@sachins-MacBook-Air.local>
2025-04-28 02:46:13 +0530

    feat: check_permission decorator

--- swvista/rbac/decorators.py
+++ swvista/rbac/decorators.py
@@ -1,6 +1,9 @@
 from functools import wraps
 
 from django.http import JsonResponse
+from django.utils import timezone
+
+from .models import AuditLog
 
 
 def session_login_required(view_func):
@@ -14,6 +17,56 @@ def session_login_required(view_func):
         if not request.session.get("user_id"):
             return JsonResponse({"error": "Authentication required."}, status=401)
         # If logged in, proceed with the original view
+        return view_func(request, *args, **kwargs)
+
+    return _wrapped_view
+
+
+def check_user_permission(permission_name):
+    """
+    Decorator factory to check if the logged-in user has a specific permission.
+    """
+
+    def decorator(view_func):
+        @wraps(view_func)
+        def _wrapped_view(request, *args, **kwargs):
+            user_permissions = request.session.get(
+                "permissions", []
+            )  # Default to empty list
+            if not request.session.get("user_id"):  # Check login first
+                return JsonResponse({"error": "Authentication required."}, status=401)
+
+            # Ensure user_permissions is a set for contains check if it's stored as a list
+            if isinstance(user_permissions, list):
+                user_permissions = set(user_permissions)
+
+            if permission_name in user_permissions:
+                return view_func(request, *args, **kwargs)
+            else:
+                return JsonResponse(
+                    {"error": "You do not have permission to access this resource."},
+                    status=403,
+                )
+
+        return _wrapped_view
+
+    return decorator
+
+
+def audit_log(view_func):
+    """
+    Decorator to log the user's actions.
+    """
+
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        AuditLog.objects.create(
+            user=request.user,
+            action=view_func.__name__,
+            timestamp=timezone.now(),
+            details=request.body,
+        )
+
         return view_func(request, *args, **kwargs)
 
     return _wrapped_view
---
+++ swvista/swvista/constants.py
@@ -0,0 +1,16 @@
+CAN_READ_USERS = "read_users"
+CAN_CREATE_USERS = "create_users"
+CAN_UPDATE_USERS = "update_users"
+CAN_DELETE_USERS = "delete_users"
+
+
+CAN_READ_ROLES = "read_roles"
+CAN_CREATE_ROLES = "create_roles"
+CAN_UPDATE_ROLES = "update_roles"
+CAN_DELETE_ROLES = "delete_roles"
+
+
+CAN_READ_PERMISSIONS = "read_permissions"
+CAN_CREATE_PERMISSIONS = "create_permissions"
+CAN_UPDATE_PERMISSIONS = "update_permissions"
+CAN_DELETE_PERMISSIONS = "delete_permissions"
