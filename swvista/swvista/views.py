cd2448a99b180ec54df198d56b007897bf84c0a9
swvista <swvista.mit@manipal.edu>
2025-06-29 21:13:18 +0530

    UI integration fixing (#8)
    * fix: integration fix with ui
    
    * fix: integration fix with ui
    
    * fix: rapid integration
    
    * fix: rapid development
    
    * fix: docker implemented
    
    * fix: docker implemented
    
    * fixing docker ignore and removing unused files
    
    * fix: added ec2 ip as allowed host
    
    ---------
    
    Co-authored-by: sachin pawar <pawardevelops@gmail.com>

--- swvista/swvista/views.py
+++ swvista/swvista/views.py
@@ -6,3 +6,7 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 def index(request):
     print(request)
     return JsonResponse({"message": "CSRF cookie set"})
+
+
+def health(request):
+    return JsonResponse({"status": "ok", "message": "Service is healthy"})
